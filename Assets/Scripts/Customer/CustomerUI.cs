ae1249e24869bb12a5c24e9f23e14ead30cfe3b7
haeun0529 <h991512@naver.com>
2026-06-25 14:26:02 +0900

    feature : 손님 머리 위 주문 UI 추가

--- Assets/Scripts/Customer/CustomerUI.cs
+++ Assets/Scripts/Customer/CustomerUI.cs
@@ -1,18 +1,42 @@
 using UnityEngine;
-using TMPro;
 using UnityEngine.UI;
 using System.Collections.Generic;
 
 public class CustomerUI : MonoBehaviour
 {
-    public GameObject uiPanel;
-    public TextMeshProUGUI orderText;
+    public GameObject speechBubble;
     public Image timerBar;
+
+    [Header("주문 아이콘 슬롯")]
+    public Image[] orderIcons;
+
+    private Color colorTteokbokki = new Color(0.9f, 0.2f, 0.2f); // 빨간색
+    private Color colorOdeng = new Color(1f, 0.6f, 0.1f);        // 주황색
+    private Color colorFry = new Color(1f, 0.9f, 0.2f);          // 노란색
 
     public void ShowOrders(List<string> orders)
     {
-        uiPanel.SetActive(true);
-        orderText.text = string.Join("\n", orders);
+        speechBubble.SetActive(true);
+
+        foreach (var icon in orderIcons)
+            icon.gameObject.SetActive(false);
+
+        for (int i = 0; i < orders.Count && i < orderIcons.Length; i++)
+        {
+            orderIcons[i].gameObject.SetActive(true);
+            orderIcons[i].color = GetColor(orders[i]);
+        }
+    }
+
+    Color GetColor(string itemName)
+    {
+        switch (itemName)
+        {
+            case "떡볶이": return colorTteokbokki;
+            case "오뎅완성": return colorOdeng;
+            case "튀김완성": return colorFry;
+            default: return Color.white;
+        }
     }
 
     public void UpdateTimer(float ratio)
@@ -25,6 +49,6 @@ public class CustomerUI : MonoBehaviour
 
     public void HideUI()
     {
-        uiPanel.SetActive(false);
+        speechBubble.SetActive(false);
     }
 }
